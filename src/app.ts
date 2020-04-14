a1e34dbd319c58a8e8bfefb7921ffca4afe61d49
juri-t <puyo24.juri.takahashi@gmail.com>
2020-04-14 23:49:10 +0900

    debug docker version

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { Express, Request, Response } from "express";
 const express = require("express");
 
 const Users = [
-  { id: 1, name: 'juri-t'}
+  { id: 1, name: 'juri-t' }
 ];
 
 export function createApp(): Express {
@@ -17,6 +17,10 @@ export function createApp(): Express {
 
   app.get("/user", (req: Request, res: Response) => {
     res.json(Users);
+  });
+
+  app.get("/health-check", (req: Request, res: Response) => {
+    res.json({ message: 'ok' })
   });
 
   return app;
